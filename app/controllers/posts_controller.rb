2d6b7da3d1edcf6cde0d6b7913f317f063647b01
ruffini47 <ruffini47@gmail.com>
2019-09-07 11:57:17 +0000

    Ruby_on_Rails_lesson_2.11終了

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,5 @@
 class PostsController < ApplicationController
   def index
-    @posts = [
-      "今日はRailsの学習です。",
-      "プログラミング、意外と簡単。",
-      "この調子で続けていこう。",
-    ]
+    @posts = Post.all
   end
 end
