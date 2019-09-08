0c87ffa3920f291db10f39f8519144ba670eb35f
ruffini47 <ruffini47@gmail.com>
2019-09-08 13:10:28 +0000

    本当のRuby_on_Rails_lesson_2.4終了

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,9 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @posts = [
+      "今日はRailsの学習です。",
+      "プログラミング、意外と簡単。",
+      "この調子で続けていこう。",
+    ]
   end
 end
