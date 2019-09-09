421ff0e611b3d82338f9141911d30456b468526f
ruffini47 <ruffini47@gmail.com>
2019-09-09 11:34:52 +0000

    Ruby_on_Rails_lesson_3.7終了

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'home#top'
   get 'top', to: 'home#top'
   get 'about', to: 'home#about'
+  get 'posts/new' ,to: 'posts#new' 
   get 'posts/index', to: 'posts#index'
   get 'posts/:id', to: 'posts#show', as: :post
 end
