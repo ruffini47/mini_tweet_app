d94d2f23071211fde165cf3dd8549e06aa74d987
ruffini47 <ruffini47@gmail.com>
2019-09-09 11:49:30 +0000

    Ruby_on_Rails_lesson_3.9終了

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,17 @@
 class PostsController < ApplicationController
+  
+  def new
+  end
+  
+  def create
+  end
+  
   def index
     @posts = Post.all
   end
   
   def show
     @post = Post.find(params[:id])
-  end
-  
-  def new
   end
 
 end
