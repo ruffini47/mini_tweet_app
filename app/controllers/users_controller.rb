9933beed760a68e7fbc7f52c3de13c61aa6b415f
ruffini47 <ruffini47@gmail.com>
2019-09-11 12:48:21 +0000

    Ruby_on_Rails_lesson_6.10終了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,15 @@
 class UsersController < ApplicationController
   
-  def new
-    @user = User.new
-  end
-  
   def index
     @users = User.all
   end
   
   def show
     @user = User.find(params[:id])
+  end
+  
+  def new
+    @user = User.new
   end
   
   def create
@@ -20,6 +20,23 @@ class UsersController < ApplicationController
     else
       render :new
     end
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    @user.name = params[:name]
+    @user.email = params[:email]
+    if @user.save
+      flash[:notice] = "ユーザー情報を編集しました。"
+      redirect_to user_url @user
+    else
+      render :edit
+    end
+
   end
   
 end
