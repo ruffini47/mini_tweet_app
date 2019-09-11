edea843b982c66d4a8e1c3e591c95c7731921adc
ruffini47 <ruffini47@gmail.com>
2019-09-11 11:22:52 +0000

    Ruby_on_Rails_lesson_6.8終了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,11 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+  
+  def create
+    @user = User.new(name: params[:name], email: params[:email])
+    @user.save
+    redirect_to user_url @user
+  end
+  
 end
