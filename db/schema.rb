4aa69ab96bcb54673f9386916151e996c58990c7
ruffini47 <ruffini47@gmail.com>
2019-09-11 13:31:28 +0000

    Ruby_on_Rails_lesson_7.1終了

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190910132331) do
+ActiveRecord::Schema.define(version: 20190911132802) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20190910132331) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
 end
