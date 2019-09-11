5fb22c17747911a2e3170b741ab763d6b8e8cb97
ruffini47 <ruffini47@gmail.com>
2019-09-11 11:44:11 +0000

    Ruby_on_Rails_lesson_6.9.2終了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+  
+  def new
+    @user = User.new
+  end
+  
   def index
     @users = User.all
   end
@@ -9,8 +14,12 @@ class UsersController < ApplicationController
   
   def create
     @user = User.new(name: params[:name], email: params[:email])
-    @user.save
-    redirect_to user_url @user
+    if @user.save
+      flash[:notice] = "ユーザーを新規登録しました！"
+      redirect_to user_url @user
+    else
+      render :new
+    end
   end
   
 end
